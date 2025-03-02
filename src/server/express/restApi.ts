6115ae7adfa8fd18bb0616370de1850a040b28ac
dipanshSaxenaVivo <dipansh.saxena@vivo.ooo>
2025-03-02 14:08:33 +0530

    fixed cors

--- src/server/express/restApi.ts
+++ src/server/express/restApi.ts
@@ -11,8 +11,17 @@ import checkoutRoutes from "./routes/checkout.routes";
 
 const app: Application = express();
 
+app.use(
+  cors({
+    origin: "https://zimozi-b21dc.web.app", // Specify your frontend URL
+    credentials: true, // Allow cookies/auth headers
+    methods: "GET,POST,PUT,DELETE", // Allowed methods
+    allowedHeaders: "Content-Type,Authorization", // Allowed headers
+  })
+);
+
 app.use(express.json({ limit: "20mb" }));
-app.use(cors());
+// app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/user", userRoutes);
