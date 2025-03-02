661d1ce1961ea6169c7cd7199c6c68455afeb196
dipanshSaxenaVivo <dipansh.saxena@vivo.ooo>
2025-03-02 13:23:20 +0530

    fixed variables

--- src/server/express/config/firebase.ts
+++ src/server/express/config/firebase.ts
@@ -5,18 +5,18 @@ var admin = require("firebase-admin");
 import { getAuth as getAdminAuth } from "firebase-admin/auth";
 // import { getAuth as getFirebaseAuth } from "firebase/auth";
 
-var serviceAccount = require("/Users/dipansh_vivo/personal/zimozi-b21dc-firebase-adminsdk-fbsvc-36feb561b0.json");
+var serviceAccount = process.env.VITE_FIREBASE_SERVICE_ACCOUNT;
 
 // Initialize Firebase (replace with your config)
 // Firebase Config (From Firebase Console)
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_FIREBASE_APP_ID,
+  measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 let app = admin.initializeApp({
